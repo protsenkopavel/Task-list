296146cef2cf850cbe132d1e28c07e57635eec6d
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-05-05 17:24:09 +0300

    project init

---
+++ src/test/java/net/protsenko/tasklist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protsenko.tasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
